b69e08db4f80f5dc9827ef16f95b9a3882713002
Marcio <marcioscar@gmail.com>
2023-12-13 16:58:17 -0300

    Cadastro treinos consertado

--- app/form-action.server.ts
+++
@@ -1,7 +0,0 @@
-import { createFormAction } from "remix-forms";
-// For Remix, import it like this
-import { redirect, json } from "@remix-run/node";
-
-const formAction = createFormAction({ redirect, json });
-
-export { formAction };
